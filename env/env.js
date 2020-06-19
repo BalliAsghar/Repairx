f850548e747527f98d2a5d1a115fede3c8c880a5
Balli <ballihacker@outlook.com>
2020-06-19 18:34:38 +0100

    oopsie

--- env/env.js
+++ env/env.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: 8080,
   DBURL: "mongodb://balli:ajkd6942@ds239309.mlab.com:39309/test12",
   secretOrKey: "adsa'd23e2d'dasda'dadada",
-  environment: "production",
+  environment: "development",
 };
