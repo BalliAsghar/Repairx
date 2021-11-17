3f11b4f2dd97112b22219dd1e883f30ec124f20a
BalliAsghar <ballihacker@outlook.com>
2021-11-17 00:04:13 +0000

    prod

--- index.js
+++ index.js
@@ -23,11 +23,14 @@ console.log(process.env);
 app.use("/api", JobRoutes);
 app.use("/user", UserRoute);
 
-app.use(express.static("client/build"));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 // Port
 const port = process.env.PORT || 8080;
